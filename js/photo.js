2f8280c14c02274f558609004db54e8521cc6f92
ericksuarez3398 <ericksuarez2016@gmail.com>
2017-04-01 09:02:35 -0700

    First Commit

---
+++ js/photo.js
@@ -0,0 +1,41 @@
+window.addEventListener("DOMContentLoaded", function(){
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+var mediaConfig = { video:true };
+var errBack = function(e) {
+  console.log('An error has occurred!', e)
+};
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    });
+}
+
+
+else if(navigator.getUserMedia) { // Standard
+    navigator.getUserMedia({ video: true }, function(stream) {
+        video.src = stream;
+        video.play();
+    }, errBack);
+} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+    navigator.webkitGetUserMedia({ video: true }, function(stream){
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+    navigator.mozGetUserMedia({ video: true }, function(stream){
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+}
+
+
+document.getElementById("check").addEventListener('click', function() {
+	context.drawImage(video, 0, 0, 640, 480);
+});
+},false);
