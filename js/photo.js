eee09eaadc5a6495d9a0d4f575426fd041a1a16d
ericksuarez3398 <ericksuarez2016@gmail.com>
2017-04-02 09:40:09 -0700

    done

--- js/photo.js
+++ js/photo.js
@@ -4,6 +4,7 @@ var context = canvas.getContext('2d');
 var video = document.getElementById('video');
 var photo = document.getElementById('photo');
 var mediaConfig = { video:true };
+var content;
 var errBack = function(e) {
   console.log('An error has occurred!', e)
 };
@@ -38,12 +39,49 @@ else if(navigator.getUserMedia) { // Standard
 
 document.getElementById("check").addEventListener('click', function() {
 	context.drawImage(video, 0, 0, 500, 400);
-  photo.setAttribute('src', canvas.toDataURL('image/png'))
+  content = canvas.toDataURL('image/png');
+  console.log(content);
+  photo.setAttribute('src', content);
+  var type = "LABEL_DETECTION";
+  var json = '{' +
+  ' "requests": [' +
+    '	{ ' +
+    '	  "image": {' +
+    '	    "content":"' + content.replace("data:image/png;base64,", "") + '"' +
+    '	  },' +
+    '	  "features": [' +
+    '	      {' +
+    '	      	"type": "' + type + '",' +
+    '			"maxResults": 200' +
+    '	      }' +
+    '	  ]' +
+    '	}' +
+    ']'
+    '}';
+
+
+  $.ajax({
+      type: 'POST',
+      url: "https://vision.googleapis.com/v1/images:annotate?key=" + "AIzaSyApLjctaIk54npL922wsGgWYaZafQ3yAws",
+      dataType: 'json',
+      data: json,
+      //Include headers, otherwise you get an odd 400 error.
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      success: function(data, textStatus, jqXHR) {
+        displayJSON(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log('ERRORS: ' + textStatus + ' ' + errorThrown);
+      }
+    });
+
 });
 },false);
-
-// Imports the Google Cloud client library
-var Vision = require('@google-cloud/vision');
+/* Imports the Google Cloud client library
+<script>var Vision = require('@google-cloud/vision');
 
 // Your Google Cloud Platform project ID
 var projectId = 'helpful-aurora-163400';
@@ -63,4 +101,4 @@ visionClient.detectLabels(fileName)
 
     console.log('Labels:');
     labels.forEach((label) => console.log(label));
-  });
+  }); </script> */
